e9509d1b944c151764df5d70488f3ce6b405fa64
Nigel Jones <nigel.l.jones+git@gmail.com>
2022-12-19 10:24:06 +0000

    Code tidying up (renames etc)
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

--- scripts/cts-analyze.py
+++ scripts/cts-analyze.py
@@ -2,69 +2,69 @@
 # SPDX-License-Identifier: CC-BY-4.0
 # Copyright Contributors to the ODPi Egeria project.
 
-import json;
-import tarfile;
-import sys;
+import json
+import tarfile
+import sys
 
-basedir='.';
-workdir=basedir+'/work';
+basedir = '.'
+workdir = basedir + '/work'
 
-# unpack reportfile
-reportfile = tarfile.open(basedir+'/export/cts.tar.gz');
-reportfile.extractall(workdir);
-reportfile.close();
+# unpack reportFile
+reportFile = tarfile.open(basedir + '/export/cts.tar.gz')
+reportFile.extractall(workdir)
+reportFile.close()
 
 # unpack detailed result file
-pdfile = tarfile.open(workdir+'/pd.tar.gz');
-pdfile.extractall(workdir);
-pdfile.close();
+pdfile = tarfile.open(workdir + '/pd.tar.gz')
+pdfile.extractall(workdir)
+pdfile.close()
 
 # Load the profile results
-with open(workdir+'/openmetadata_cts_summary.json') as f:
-    summary = json.load(f);
+with open(workdir + '/openmetadata_cts_summary.json') as f:
+    summary = json.load(f)
 
-fullgood=0;
-fullbad=0;
+totalGood = 0
+totalBad = 0
 
 # Assume standard CTS
-profileSummaries=summary['testLabSummary']['testSummariesFromWorkbenches'][0]['profileSummaries'];
+profileSummaries = summary['testLabSummary']['testSummariesFromWorkbenches'][0]['profileSummaries']
 
 for p in profileSummaries:
-    #print(p);
-    name=p['name'];
-    priority=p['profilePriority'];
-    conformanceStatus=p['conformanceStatus'];
+    # print(p);
+    name = p['name']
+    priority = p['profilePriority']
+    conformanceStatus = p['conformanceStatus']
 
     # Capture any errors
-    fname=name.replace(" ", "_");
-    with open(workdir+'/profile-details/'+fname+'.json') as f:
-        details = json.load(f);
-        totalgood=0;
-        totalbad=0;
+    fname = name.replace(" ", "_")
+    with open(workdir + '/profile-details/' + fname + '.json') as f:
+        details = json.load(f)
+        good = 0
+        bad = 0
         try:
-            results=details['profileResult']['requirementResults'];
+            results = details['profileResult']['requirementResults']
             for r in results:
                 try:
-                    totalgood+=len(r.get('positiveTestEvidence'));
+                    good += len(r.get('positiveTestEvidence'))
                 # if empty list - ie all good
                 except Exception:
                     pass
                 try:
-                    totalbad+=len(r.get('negativeTestEvidence'));
+                    bad += len(r.get('negativeTestEvidence'))
                 # if empty list - ie no fails
                 except Exception:
                     pass
         # If this function isn't supported at all
         except Exception:
             pass
-    fullgood+=totalgood;
-    fullbad+=totalbad;
+    totalGood += good
+    totalBad += bad
 
-    print ("%30s %17s %26s [ %6i / %6i ]" % (name,priority,conformanceStatus,totalgood,totalbad));
+    print("%30s %17s %26s [ %6i / %6i ]" % (name, priority, conformanceStatus, good, bad))
 
-if (fullbad==0):
-    print("\nPASS [%i/%i]" % (fullgood,fullbad));
+if totalBad == 0:
+    print("\nPASS [%i/%i]" % (totalGood, totalBad))
 else:
-    print("\nFAIL [%i/%i]" % (fullgood,fullbad));
+    print("\nFAIL [%i/%i]" % (totalGood, totalGood))
 
-sys.exit(fullbad!=0);
+sys.exit(totalBad != 0)
